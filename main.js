d4337d60639fb4323692be5ddeb109716f51eec0
Farshad <FarshadJanmohammadi@gmail.com>
2022-02-13 15:19:50 +0330

    edit

--- main.js
+++ main.js
@@ -21,8 +21,12 @@ const counterHandler = () => {
 };
 counterHandler();
 
-const clickHandler = () => {
+const clickHandler = (event) => {
+  const audioShowAll = new Audio();
+  audioShowAll.src = "./css/sounds/clickShowAll.mp3";
+  audioShowAll.play();
   document.styleSheets[0].cssRules[12].style.setProperty("display", "flex");
+  event.srcElement.disabled = true;
 };
 const mouseEnterHandler = (event) => {
   if (window.innerWidth >= 570) {
@@ -43,8 +47,11 @@ const mouseLeaveHandler = (event) => {
 
 const closeClickHandler = (event) => {
   console.log(event);
-  clickHandler();
+  document.styleSheets[0].cssRules[12].style.setProperty("display", "flex");
   if (event.target.localName == "span") {
+    const audio = new Audio();
+    audio.src = "./css/sounds/click.mp3";
+    audio.play();
     event.path[2].remove();
   } else {
     return;
